63402a1407f2040124275345dc8f84db21065693
tushar.dhuri <tushar.dhuri@capitaassetservices.ie>
2018-08-14 23:17:08 +0100

    Program1 : Creating a thread with Thread Class

---
+++ Program1.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading;
+
+
+//Creating a thread with a thread class
+namespace Chapter1
+{
+public static class Program
+{
+for(int i=0;i<10;i++)
+{
+Console.WriteLine
+
+}
+    
+}
+
+}
