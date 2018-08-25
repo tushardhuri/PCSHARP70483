f402f6c8eddd8e64b7bfe6972f6fb3bb203d3b2e
tushar.dhuri <tushar.dhuri@capitaassetservices.ie>
2018-08-25 21:59:33 +0100

    Program 2 to Program 5

---
+++ AllProgramSolution/Practice70483/Program3_UsingParameterizedThreadStart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Practice70483
+{
+    //Listing 1-3 Using the Parameterized Thread Start
+    public static class Program3_UsingParameterizedThreadStart
+    {
+        public static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine("Thread Proc : {0}", i);
+                Thread.Sleep(1000);
+            }
+        }
+
+        //public static void Main()
+        //{
+        //    Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));                       
+        //    t.Start(5);
+        //    t.Join();
+        //}
+    }
+}
